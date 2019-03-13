2cdd4cead02d8e10be3578f2fea0ca177e7366ce
ben lamothe <bl@732.one>
2019-03-13 01:22:10 -0400

    pass objects that are already cbor nodes through safewrap unmodified

--- safewrap/safewrap.go
+++ safewrap/safewrap.go
@@ -18,8 +18,18 @@ func (sf *SafeWrap) WrapObject(obj interface{}) *cbornode.Node {
 		return nil
 	}
 
-	node, err := cbornode.WrapObject(obj, multihash.SHA2_256, -1)
-	sf.Err = err
+	var (
+		node *cbornode.Node
+		err  error
+	)
+
+	if cbor, ok := obj.(cbornode.Node); ok {
+		node = &cbor
+	} else {
+		node, err = cbornode.WrapObject(obj, multihash.SHA2_256, -1)
+		sf.Err = err
+	}
+
 	return node
 }
 
