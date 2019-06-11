86326950b03c0c9d93be7d81a97dcd8421a384ec
Wes Morgan <wesmorgan@icloud.com>
2019-06-11 15:44:45 -0600

    Allow getting tree for an arbitrary tip

--- chaintree/chaintree.go
+++ chaintree/chaintree.go
@@ -168,6 +168,17 @@ func (ct *ChainTree) Id() (string, error) {
 	return root.Id, nil
 }
 
+func (ct *ChainTree) TreeAt(tip cid.Cid) (*dag.Dag, error) {
+	root, err := ct.getRootAt(tip)
+	if err != nil {
+		return nil, &ErrorCode{Code: ErrUnknown, Memo: fmt.Sprintf("error getting root node for tip %v: %v", tip, err.Error())}
+	}
+	if root.Tree == nil {
+		return nil, &ErrorCode{Code: ErrInvalidTree, Memo: "tree link is nil"}
+	}
+	return ct.Dag.WithNewTip(*root.Tree), nil
+}
+
 // Tree returns just the tree portion of the ChainTree as a pointer to its DAG
 func (ct *ChainTree) Tree() (*dag.Dag, error) {
 	root, err := ct.getRoot()
@@ -359,9 +370,26 @@ func (ct *ChainTree) getRoot() (*RootNode, error) {
 
 	err = cbornode.DecodeInto(unmarshaledRoot.RawData(), root)
 	if err != nil {
-		return nil, &ErrorCode{Code: ErrInvalidTree, Memo: fmt.Sprintf("error converting root: %v", err)}
+		return nil, &ErrorCode{Code: ErrInvalidTree, Memo: fmt.Sprintf("error decoding root: %v", err)}
 	}
+
 	root.cid = ct.Dag.Tip
 	ct.root = root
+	return root, nil
+}
+
+func (ct *ChainTree) getRootAt(tip cid.Cid) (*RootNode, error) {
+	unmarshaledRoot, err := ct.Dag.Get(tip)
+	if unmarshaledRoot == nil || err != nil {
+		return nil, &ErrorCode{Code: ErrInvalidTree, Memo: fmt.Sprintf("error: invalid tip or missing root: %v", err)}
+	}
+
+	root := &RootNode{}
+
+	err = cbornode.DecodeInto(unmarshaledRoot.RawData(), root)
+	if err != nil {
+		return nil, &ErrorCode{Code: ErrInvalidTree, Memo: fmt.Sprintf("error decoding root: %v", err)}
+	}
+
 	return root, nil
 }
