8cff0a2a586a8cc2e998dd711e5c1704bf9ffab0
Topper Bowers <topper@toppingdesign.com>
2019-09-09 10:02:03 +0200

    bug fix, we also want to make sure taht in a NotFound, we allow the process to complete

--- dag/dag.go
+++ dag/dag.go
@@ -233,7 +233,7 @@ func (d *Dag) nodeAndDescendants(ctx context.Context, node format.Node, collecto
 			continue
 		}
 		linkNode, err := d.store.Get(ctx, link.Cid)
-		if err != nil {
+		if err != nil && err != format.ErrNotFound {
 			return fmt.Errorf("error getting link: %v", err)
 		}
 		if linkNode == nil {
@@ -450,7 +450,7 @@ func (d *Dag) dumpNode(ctx context.Context, node format.Node, isLink bool) inter
 	if err := cbornode.DecodeInto(node.RawData(), &nodeData); err != nil {
 		panic(err)
 	}
-	
+
 	switch nodeData.(type) {
 	case map[interface{}]interface{}:
 		nodeMap := nodeData.(map[interface{}]interface{})
